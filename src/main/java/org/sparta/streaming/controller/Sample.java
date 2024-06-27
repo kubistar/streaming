d9edcc64ffdf574125c29447dbd20a2255589dc8
kubistar <dbtmdal17@gmail.com>
2024-06-28 02:20:33 +0900

    도커 컴포즈

--- src/main/java/org/sparta/streaming/controller/Sample.java
+++ src/main/java/org/sparta/streaming/controller/Sample.java
@@ -1,4 +1,14 @@
 package org.sparta.streaming.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Sample {
+
+    @RequestMapping("/sample")
+    public String greeting(){
+        return "sample!!";
+    }
+
 }
